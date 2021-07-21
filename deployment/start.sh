512c8977adebeb006e8706ee0bd48a83fe517ce9
antoine_drabble_avalia <antoine@avalia.expert>
2021-07-22 00:54:38 +0200

    Update docker config

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd apps/traefik
-docker-compose up -d
+docker-compose up -d --remove-orphans
 
 cd ../mongo
-docker-compose up -d
+docker-compose up -d --remove-orphans
 
 cd ../shlong
-docker-compose up -d
+docker-compose up -d --remove-orphans
