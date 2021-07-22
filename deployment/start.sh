ff31c5399fd466ba8b72d21cb0824cf0535214c1
Antoine Drabble <antoine.drabble@gmail.com>
2021-07-22 16:09:29 +0200

    Externalise mongo

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd apps/traefik
-docker-compose up -d --remove-orphans
+docker-compose up -d
 
-cd ../mongo
-docker-compose up -d --remove-orphans
+#cd ../mongo
+#docker-compose up -d
 
 cd ../shlong
-docker-compose up -d --remove-orphans
+docker-compose up -d
