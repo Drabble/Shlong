2a33a643e9b35046956eb3650563c1d8eb166a1b
antoine_drabble_avalia <antoine@avalia.expert>
2021-07-23 21:30:26 +0200

    Name not required

--- api/Model/User.js
+++ api/Model/User.js
@@ -13,11 +13,11 @@ const userSchema = new mongoose.Schema({
   },
   firstName: {
     type: String,
-    required: true,
+    required: false,
   },
   lastName: {
     type: String,
-    required: true,
+    required: false,
   },
   image: {
     type: String,
